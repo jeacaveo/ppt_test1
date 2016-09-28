2658c7f3c1afa4a93a93f060a4ded792f4aa7d07
Jean Ventura <jv@venturasystems.net>
2016-09-28 13:35:07 -0400

    Added Tag model.

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from blog import models
 
+
 class AuthorTestCase(TestCase):
     def setUp(self):
         models.Author.objects.create(name="J. Snow")
@@ -14,9 +15,30 @@ class AuthorTestCase(TestCase):
 
         # When
         author = models.Author.objects.get(name=author_name)
-        not_author = models.Author.objects.filter(name="Tyrion").first()
+        not_author = models.Author.objects.filter(name="not_author").first()
 
         # Then
         self.assertEqual(author_count, 2)
         self.assertEqual(author.name, author_name)
         self.assertFalse(not_author)
+
+
+class TagTestCase(TestCase):
+    def setUp(self):
+        models.Tag.objects.create(name="cooking")
+        models.Tag.objects.create(name="sports")
+
+    def test_author_object(self):
+        """ Test proper creation of Tags in database. """
+        # Given
+        tag_count = models.Tag.objects.count()
+        tag_name = "cooking"
+
+        # When
+        tag = models.Tag.objects.get(name=tag_name)
+        not_tag = models.Tag.objects.filter(name="not_tag").first()
+
+        # Then
+        self.assertEqual(tag_count, 2)
+        self.assertEqual(tag.name, tag_name)
+        self.assertFalse(not_tag)
